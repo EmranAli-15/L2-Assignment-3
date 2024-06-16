b918f0230e29042125dd24559d1a00bf931b2609
EmranAli-15 <mohammademranali15@gmail.com>
2024-06-16 22:06:32 +0600

    password hashed

--- src/app/modules/user/user.model.ts
+++ src/app/modules/user/user.model.ts
@@ -1,5 +1,7 @@
 import { Schema, model } from "mongoose";
 import { TUser } from "./user.interface";
+import bcrypt from 'bcrypt';
+import config from "../../config";
 
 const userSchema = new Schema<TUser>(
     {
@@ -35,5 +37,17 @@ const userSchema = new Schema<TUser>(
         timestamps: true
     }
 );
+
+userSchema.pre('save', async function (next) {
+    const user = this;
+    user.password = await bcrypt.hash(user.password, Number(config.saltRounds));
+
+    next();
+});
+
+userSchema.post('save', function (next) {
+    const user = this;
+    user.password = "";
+});
 
 export const User = model<TUser>('User', userSchema);
